ed0af9febb90a2fd497f87b7952c4c29f325f09e
王军伟 <wayne214@163.com>
2017-09-27 13:27:48 +0800

    添加货源详情页面

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -120,8 +120,8 @@ class Login extends BaseContainer {
     constructor(props) {
         super(props);
         this.state = {
-            phoneNumber: __DEV__ ? '18888888888' : '',
-            password: __DEV__ ? '123456' : '',
+            phoneNumber: __DEV__ ? '13120382724' : '',
+            password: __DEV__ ? 'asd123' : '',
             loading: false,
         };
         this.loginSecretCode = this.loginSecretCode.bind(this);
