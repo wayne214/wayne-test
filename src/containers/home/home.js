e2be379903d88a3d8d6b229a66129c4ced7eda26
常会军 <chjwrr@163.com>
2017-10-12 16:48:56 +0800

    tabbar适配iPhoneX

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -25,7 +25,7 @@ import { NavigationActions } from 'react-navigation';
 
 import HomeCell from './components/homeCell';
 import WeatherCell from './components/weatherCell';
-
+import * as ConstValue from '../../constants/constValue';
 import {
     WHITE_COLOR,
     BLUE_TEXT_COLOR,
@@ -167,7 +167,7 @@ const styles = StyleSheet.create({
         flex: 1,
         ...Platform.select({
             ios: {
-                paddingTop: 15,
+                paddingTop: ConstValue.StatusBar_Height,
             },
             android: {
                 paddingTop: 0,
