905a2d666e247774f24bdf31bf9271ce3053b1f0
liqingqing <qingqingyeshi311@163.com>
2017-10-18 10:40:57 +0800

    AnExampleReactPackage.java

--- android/app/src/main/java/com/xescm/driver/AnExampleReactPackage.java
+++ android/app/src/main/java/com/xescm/driver/AnExampleReactPackage.java
@@ -23,10 +23,10 @@ public class AnExampleReactPackage implements ReactPackage {
         return nativeModules;
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
+//    @Override
+//    public List<Class<? extends JavaScriptModule>> createJSModules() {
+//        return Collections.emptyList();
+//    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
