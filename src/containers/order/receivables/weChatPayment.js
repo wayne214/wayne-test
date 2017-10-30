aeeefb6cdb914cf40c3039d36bf917e6871301db
席智欣 <947538363@qq.com>
2017-10-30 18:40:16 +0800

    添加微信支付界面

--- src/containers/order/receivables/weChatPayment.js
+++ src/containers/order/receivables/weChatPayment.js
@@ -10,13 +10,14 @@ import {
     StyleSheet,
     Platform,
     TouchableOpacity,
-    Dimensions
+    Dimensions,
+    Image
 } from 'react-native';
 
-import DetailsCell from '../../../common/source/detailsCell';
 import * as StaticColor from '../../../constants/staticColor';
 import * as ConstValue from '../../../constants/constValue';
-const {width} = Dimensions.get('window');
+import qrCode from '../../../../assets/order/qrcode.png';
+import StaticImage from '../../../constants/staticImage';
 class WeChatPayment extends Component {
     constructor(props) {
         super(props);
@@ -31,23 +32,71 @@ class WeChatPayment extends Component {
                 <View style={styles.container}>
                     <View style={styles.contentContainer}>
                         <View style={styles.leftContainer}>
-                            <TouchableOpacity
-                                onPress={() => {
-                                    navigator.goBack();
-                                }}
-                            >
-                                <Text style={styles.leftTextStyle}>&#xe662;</Text>
-                            </TouchableOpacity>
+                            {
+                                2 === 1 ?
+                                    <View>
+                                        <TouchableOpacity
+                                            onPress={() => {
+                                                navigator.goBack();
+                                            }}
+                                        >
+                                            <Text style={styles.leftTextStyle}>&#xe662;</Text>
+                                        </TouchableOpacity>
+                                    </View> : null
+                            }
                         </View>
                         <View style={styles.centerContainer}>
                             <Text style={styles.centerTextStyle}>收款</Text>
                         </View>
                         <View style={styles.rightContainer}>
+                            {
+                                2 === 1 ? null :
+                                    <View>
+                                        <TouchableOpacity
+                                            onPress={() => {
+                                                console.log('完成按钮点击事件');
+                                            }}
+                                        >
+                                            <Text style={styles.buttonText}>完成</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                            }
                         </View>
                     </View>
                 </View>
                 <View style={styles.paymentView}>
-
+                    <View style={styles.paymentTitleView}>
+                       <View style={styles.flexDirection}>
+                           <Text style={styles.paymentIcon}>&#xe671;</Text>
+                           <Text style={styles.paymentText}>微信支付</Text>
+                       </View>
+                    </View>
+                        {
+                            2 === 1 ?
+                            <View>
+                                <Text style={styles.amountText}>￥230.00</Text>
+                                <Text style={styles.tip}>二维码有效期是10分钟</Text>
+                                <View style={styles.imageView}>
+                                    <Image source={qrCode} style={styles.image}/>
+                                </View>
+                                <View style={{alignItems:'center'}}>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            console.log('刷新二维码');
+                                        }}
+                                    >
+                                        <View style={styles.flexDirection}>
+                                            <Text style={styles.tipIcon}>&#xe66b;</Text>
+                                            <Text style={styles.tipText}>手动刷新二维码</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            </View> :
+                            <View style={styles.content}>
+                                <Image source={StaticImage.finishIcon} />
+                                <Text style={styles.success}>支付成功</Text>
+                            </View>
+                        }
                 </View>
                 <View style={styles.bottomView}>
                     <View style={styles.titleView}>
@@ -156,6 +205,18 @@ const styles =StyleSheet.create({
         marginLeft: 15,
         paddingTop: 11,
     },
+    paymentIcon:{
+        fontFamily: 'iconfont',
+        color: StaticColor.BLUE_BACKGROUND_COLOR,
+        fontSize: 18,
+        alignSelf: 'center',
+    },
+    tipIcon:{
+        fontFamily: 'iconfont',
+        color: '#A0A0A0',
+        fontSize: 15,
+        alignSelf: 'center',
+    },
     titleIcon:{
         fontFamily: 'iconfont',
         color: '#A0A0A0',
@@ -166,6 +227,11 @@ const styles =StyleSheet.create({
         fontSize: 18,
         color: StaticColor.LIGHT_BLACK_TEXT_COLOR,
         marginLeft: 8,
+    },
+    paymentText: {
+        fontSize: 16,
+        color: StaticColor.LIGHT_BLACK_TEXT_COLOR,
+        marginLeft: 5,
     },
     flexDirection: {
         flexDirection: 'row',
@@ -180,6 +246,60 @@ const styles =StyleSheet.create({
         color: StaticColor.GRAY_TEXT_COLOR,
         paddingBottom: 5,
     },
+    paymentTitleView: {
+        paddingTop: 14,
+        paddingBottom: 14,
+        backgroundColor: StaticColor.TITLE_BACKGROUND_COLOR,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginLeft: 15,
+    },
+    amountText: {
+        fontSize: 25,
+        color: StaticColor.LIGHT_BLACK_TEXT_COLOR,
+        alignSelf: 'center',
+        padding: 20,
+    },
+    tipText: {
+        fontSize: 14,
+        color: StaticColor.COLOR_LIGHT_GRAY_TEXT,
+        marginLeft: 10,
+    },
+    tip: {
+        fontSize: 14,
+        color: StaticColor.COLOR_LIGHT_GRAY_TEXT,
+        alignSelf: 'center',
+    },
+    imageView: {
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    image: {
+        width: 150,
+        height: 150,
+        alignSelf: 'center'
+    },
+    success: {
+        fontSize: 16,
+        color: StaticColor.LIGHT_BLACK_TEXT_COLOR,
+        marginTop: 15,
+    },
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        marginBottom: 10
+    },
+    buttonText: {
+        fontSize: 16,
+        color: StaticColor.WHITE_COLOR,
+        textAlign: 'center',
+        marginRight: 10,
+        marginTop: 10,
+    }
 });
 
 function mapStateToProps(state){
