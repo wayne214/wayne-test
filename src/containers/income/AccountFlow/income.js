a4912397629855836ef3d3d1ab5e4ed2e854ad43
席智欣 <947538363@qq.com>
2017-11-06 14:44:27 +0800

    修改标题布局

--- src/containers/income/AccountFlow/income.js
+++ src/containers/income/AccountFlow/income.js
@@ -18,10 +18,12 @@ import NavigationBar from '../../../common/navigationBar/navigationBar';
 import IncomeCell from './cell/incomeCell';
 import * as API from '../../../constants/api';
 import Storage from '../../../utils/storage';
+import * as StaticColor from '../../../constants/staticColor';
 import {Geolocation} from 'react-native-baidu-map-xzx';
 import ReadAndWriteFileUtil from '../../../utils/readAndWriteFileUtil';
 import HTTPRequest from '../../../utils/httpRequest';
 import StaticImage from '../../../constants/staticImage';
+import * as ConstValue from '../../../constants/constValue';
 
 const {height, width} = Dimensions.get('window');
 let currentTime = 0;
@@ -29,19 +31,53 @@ let lastTime = 0;
 let locationData = '';
 
 const styles = StyleSheet.create({
-    title: {
+    allContainer: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+    },
+    container: {
         ...Platform.select({
             ios: {
-                marginTop: 44,
+                height: ConstValue.NavigationBar_StatusBar_Height,
             },
             android: {
-                marginTop: 20,
+                height: 50,
             },
         }),
-        textAlign: 'center',
-        color: '#ffffff',
-        fontSize: 18,
         backgroundColor: 'transparent',
+        width: width,
+    },
+    titleContainer: {
+        flex: 1,
+        ...Platform.select({
+            ios: {
+                paddingTop: ConstValue.StatusBar_Height,
+            },
+            android: {
+                paddingTop: 0,
+            },
+        }),
+        flexDirection: 'row',
+    },
+    leftContainer: {
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    centerContainer: {
+        flex: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 7,
+    },
+    rightContainer: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 18,
+        color: StaticColor.WHITE_COLOR,
     },
 });
 
@@ -84,7 +120,6 @@ export default class Income extends Component {
             url: API.API_AC_BALANCE + global.phone,
             params: {},
             loading: () => {
-
             },
             success: (response) => {
                 lastTime = new Date().getTime();
@@ -95,79 +130,72 @@ export default class Income extends Component {
                 })
             },
             error: (err) => {
-
             },
             finish: () => {
-
             },
-
         })
-
     }
-
 
     render() {
         const navigator = this.props.navigation;
         const {accountMoney} = this.state;
-
+        // 标题布局
+        const TitleView =
+            <View style={styles.container}>
+                <View style={styles.titleContainer}>
+                    <View style={styles.leftContainer}>
+                    </View>
+                    <View style={styles.centerContainer}>
+                        <Text style={styles.title}>我的收入</Text>
+                    </View>
+                    <View style={styles.rightContainer}>
+                    </View>
+                </View>
+            </View>;
         return (
-            <View style={{flex: 1, backgroundColor: '#f5f5f5'}}>
-
+            <View style={styles.allContainer}>
                 <ImageBackground style={{height: height / 3 + 10, width}} source={StaticImage.IncomeBgimage}>
-
-                    <Text style={styles.title}>我的收入</Text>
+                    {TitleView}
                     <Text style={{
                             fontSize: 18,
-                            color: '#FFFFFF',
+                            color: StaticColor.WHITE_COLOR,
                             marginLeft: 10,
                             backgroundColor: 'transparent',
                             marginTop: 70,
                         }}>账户余额(元)</Text>
                     <Text style={{
                             fontSize: 58,
-                            color: '#FFFFFF',
+                            color: StaticColor.WHITE_COLOR,
                             marginLeft: 10,
                             marginTop: 10,
                             backgroundColor: 'transparent',
                         }}>{accountMoney}</Text>
-
                 </ImageBackground>
-
-                <View style={{flex: 1, backgroundColor: '#f5f5f5'}}>
+                <View style={styles.allContainer}>
                     <View style={{marginTop: 10}}/>
-
                     <IncomeCell leftIcon="&#xe622;" content={'业务明细'} clickAction={() => {
                         navigator.navigate('BusinessDetail');
                     }}/>
-
                     <View style={{marginTop: 1}}/>
                     <IncomeCell leftIcon="&#xe625;" content={'账户流水'} clickAction={() => {
                         navigator.navigate('BillWaterPage')
                     }}/>
-
-
                     <View style={{marginTop: 10}}/>
-
                     {/*<IncomeCell leftIcon="&#xe623;" content={'提现'}*/}
                                 {/*clickAction={() => {*/}
                                     {/*// 先判断有没有添加银行卡，如果没有，则跳转到添加银行卡页面。*/}
                                     {/*navigator.navigate('Withdrawals')*/}
                     {/*}}/>*/}
                     {/*<View style={{marginTop: 1}}/>*/}
-
                     <IncomeCell leftIcon="&#xe624;" content={'我的银行卡'}
                     iconColor="rgb(250,128,10)"
                     clickAction={() => {
                         navigator.navigate('MyBankCard')
                     }}/>
-
-
                 </View>
             </View>
         );
     }
-
-
 }
 
 
