78d4a68f8652b7b16d1f64b32032d4fa746eb392
wanglei <wanglei1005@qq.com>
2017-12-19 10:47:21 +0800

    merge

--- src/constants/actionType.js
+++ src/constants/actionType.js
@@ -41,3 +41,5 @@ export const UPDATE_IMAGES = 'UPDATE_IMAGES'; // 清空图片
 export const ACTION_VOICE_SWITCH = 'ACTION_VOICE_SWITCH'; // 语音播报
 
 export const ACTION_SET_CHARACTER = 'ACTION_SET_CHARACTER'; // 账户-角色
+
+export const ACTION_SET_CURRENT_CHARACTER = 'ACTION_SET_CURRENT_CHARACTER'; // 账户-当前选择角色
